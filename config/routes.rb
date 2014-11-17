da0a36185078e026673e203718279612504f53fd
Dan Frenette <paradox815@gmail.com>
2014-11-17 11:31:59 -0500

    Users can sign up with Monban

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/writing_area'
+  resource :session, only: [:create, :new, :destroy]
+  resources :users, only: [:new, :create]
 
-  get 'static_pages/home'
-
-  get 'static_pages/hel'
-
-  get "static_pages/home"
-  
+  root to: "sessions#new"
 end
