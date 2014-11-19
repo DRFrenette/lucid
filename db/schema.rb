725ac0ac166c8c57fbd54fbc6d768cf30893254d
Dan Frenette <paradox815@gmail.com>
2014-11-19 10:48:07 -0500

    Add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117190050) do
+ActiveRecord::Schema.define(version: 20141119152552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,12 @@ ActiveRecord::Schema.define(version: 20141117190050) do
   add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                       null: false
-    t.string   "password_digest",             null: false
-    t.integer  "level",           default: 1, null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.integer  "level",           default: 1,     null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
