d3676a7e20fd8a8da16ef1e684f56635d6945609
Dan Frenette <paradox815@gmail.com>
2014-11-26 21:46:29 -0500

    Add prompts index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141204211152) do
+=======
+ActiveRecord::Schema.define(version: 20141127021749) do
+>>>>>>> Add prompts index
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +38,13 @@ ActiveRecord::Schema.define(version: 20141204211152) do
   end
 
   add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
+
+  create_table "prompts", force: true do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                           null: false
