5cd9e233e11699c24083c13a6770d6f469f724d0
Dan Frenette <paradox815@gmail.com>
2014-11-26 22:14:02 -0500

    Users can create prompts, can see all prompts

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -3,4 +3,23 @@ class PromptsController < ApplicationController
   def index
     @promtps = Prompt.all
   end
+
+  def new
+    @prompt = Prompt.new
+  end
+
+  def create
+    @prompt = Prompt.create(prompt_params)
+    if @prompt.save?
+      redirect_to prompts_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def prompt_params
+    params.require(:prompt).permit(:title, :body)
+  end
 end
