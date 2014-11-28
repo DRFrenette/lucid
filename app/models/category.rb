01c53e7511003c27caf1ddf46ed7eabceb4b8abe
Dan Frenette <paradox815@gmail.com>
2014-11-28 01:49:42 -0500

    AR association prompts/categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,9 @@
 class Category < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :guidelines, presence: true
   validates :user_id, presence: true
 
   belongs_to :user
+  has_many :prompts
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -4,5 +4,6 @@ class Prompt < ActiveRecord::Base
   validates :body, presence: true
 
   belongs_to :user
+  belongs_to :category
   has_many :comments, dependent: :destroy
 end
