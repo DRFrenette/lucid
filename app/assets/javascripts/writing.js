35240974b88ac1b96226187530e02d18d76576a0
Dan Frenette <paradox815@gmail.com>
2014-12-04 14:06:18 -0500

    Add word count to writing area

--- app/assets/javascripts/writing.js
+++ app/assets/javascripts/writing.js
@@ -1,9 +1,6 @@
 function setWordCount() {
   var wordCount;
   var body = $(".writing-area textarea").val().trim()
-    var words = string.replace(/\s+/gi, ' ').split(' ').length
-    var chars = string.length;
-  if(!chars)words=0;
 
   if (body === "") {
     wordCount = 0;
@@ -11,13 +8,10 @@ function setWordCount() {
     wordCount = body.split(/\s+/).length;
   }
 
-  $(".word-count").val(wordCount);
+  $(".word-count").text(wordCount);
 }
 
 $(function() {
-  $(".word-count").hover(function() {
-    $(this).find(".words").toggleClass("hidden")
-  })
-
-  $("textarea").on("input", setWordCount)
+  $("textarea").on('input', setWordCount)
 })
+
--- db/migrate/20141203153120_create_notecards.rb
+++
@@ -1,8 +0,0 @@
-class CreateNotecards < ActiveRecord::Migration
-  def change
-    create_table :notecards do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
